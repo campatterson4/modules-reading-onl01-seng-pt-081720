36831b9a546f98a676a07895693fcbad854baff7
Camryn Patterson <camrynpatterson@yahoo.com>
2020-09-22 06:58:04 +0000

    Automatically backed up by Learn

---
+++ lib/fancy_dance.rb
@@ -0,0 +1,12 @@
+module FancyDance
+  module InstanceMethods             
+    def superjump 
+      "I'm just one jumping instance"
+    end
+  end
+  module ClassMethods                
+    def megajump 
+      "I'm a whole class of MOFO's jumping"
+    end
+  end 
+end
