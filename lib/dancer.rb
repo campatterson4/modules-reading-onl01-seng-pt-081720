1974523bd70a127deac45ae545f2c1f8eb33848c
Camryn Patterson <camrynpatterson@yahoo.com>
2020-09-22 07:01:01 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -3,10 +3,9 @@ require_relative './dance_module.rb'
 
 class Dancer
   include Dance
- 
-  attr_accessor :name
- 
-  def initialize(name)
+  extend MetaDancing
+ attr_accessor :name
+ def initialize(name)
     @name = name
   end
 end
